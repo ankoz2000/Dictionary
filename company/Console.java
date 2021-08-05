d48fcc5b730fa51e11067bc95f380a9e81036cf3
akozlov <akozlov@mmtr.ru>
2021-08-05 15:12:18 +0300

    programm's structure description (just about)

---
+++ company/Console.java
@@ -0,0 +1,26 @@
+package com.company;
+
+// Пока делаю ключ-значения интовые, позже переделаю на String
+
+public class Console {
+    private int key;
+    private int value;
+    private String[] file_path;
+
+    private void accept(String[] answer) {
+
+    }
+
+    private static void show_menu() {
+        System.out.println("MENU:");
+        System.out.println();
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        //while(true) {
+            show_menu();
+        //}
+    }
+
+}
